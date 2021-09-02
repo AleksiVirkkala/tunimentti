78b8eb4d214de309cbc10722444c05bb37443ccb
AleksiVirkkala <aleksi.virkkala@gmail.com>
2021-09-02 22:48:54 +0300

    feat: :children_crossing: improve tunimentti

--- src/components/TunimenttiInfo.vue
+++ src/components/TunimenttiInfo.vue
@@ -1,10 +1,34 @@
 <template>
   <div class="col">
     <div class="image">
-      <img class="tunimentti" src="/img/tunimentti.jpg">
+      <img class="tunimentti" src="/img/tunimentti.png">
     </div>
+    <h1 class="title">Tunimentin huutokauppa</h1>
     <div class="description">
-      {{ productDescription }}
+      <p>
+        Korkeakoulutettujenyhteisö on päätynyt huutokauppaamaan tunimentin päästösyistä. Tarjoukset Tunimentista otetaan vastaan sähköpostilla osoitteeseen <strong>tarjoukset@tunimentti.com</strong> tai alta löytyvään julkiseen chattiin.
+      </p>
+      <div>
+        <strong>Mitat:</strong>
+        <li>
+          leveys <strong>1,5m</strong>
+        </li>
+        <li>
+          syvyys: <strong>0,40m</strong>
+        </li>
+        <li>
+          Korkeus: <strong>13,5m</strong>
+        </li>
+      </div>
+      <p>
+        Tiedämme Tunimentin olevan suuri nähtävyys, mutta vaikka Tunimentti on kaupan, korkeakoulutettujenyhteisö pyytää kaikkia kohtelemaan sitä arvokkaasti ja annetaan sen olla rauhassa sen nykyisessä kotipaikassa.
+      </p>
+      <p>
+        Otamme tarjouksia vastaan <strong>8.9.2021 23:59</strong> saakka.
+      </p>
+      <p>
+        Tarjousajan päätyttyä Tampereen korkeakouluyhteisön täytyy hyväksyä hinta ja antaa lupa myynnille. Pidämme oikeudet olla myymättä tuotetta jos tarjous ei ole riittävä.
+      </p>
     </div>
   </div>
 </template>
@@ -23,22 +47,34 @@ export default defineComponent({
 
 <style scoped>
   .col {
-    padding: 30px 20px;
+    padding: 30px;
     display: flex;
     flex-direction: column;
+    position: relative;
+    overflow: hidden;
+  }
+  .title {
+    margin-top: 0;
   }
   .description {
     white-space: pre-line;
-    padding-top: 20px;
   }
   .image {
     display: flex;
     justify-content: center;
   }
   .tunimentti {
-    object-fit: cover;
-    height: 350px;
-    width: 100%;
-    max-width: 500px;
+    height: 700px;
+    top: 120px;
+    opacity: 0.2;
+    position: absolute;
+    transform-origin: center;
+    animation: bigtuni 2s;
   }
+
+  @keyframes bigtuni {
+  0%   { transform: scale(1.4); opacity: 0; }
+  100% { transform: scale(1); opacity: 0.2; }
+}
+
 </style>

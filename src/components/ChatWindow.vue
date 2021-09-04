b1820b877d40a9fccaa5978ef54c5ec2d1f9d95f
AleksiVirkkala <aleksi.virkkala@gmail.com>
2021-09-04 15:06:13 +0300

    feat: :sparkles: combine mulitple messages

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -7,7 +7,7 @@
           v-for="doc in formattedDocuments"
           :key="doc.id"
           :name="doc.name"
-          :text="[ doc.message ]"
+          :text="doc.messages"
           avatar="https://cdn.quasar.dev/img/avatar3.jpg"
           :stamp="doc.createdAt"
           :sent="false"
@@ -15,8 +15,8 @@
           bg-color="primary"
         >
           <template #avatar>
-            <span class="q-mr-sm text-h4">
-              ❤️
+            <span class="q-mr-sm text-h5">
+              {{ doc.emoji }}
             </span>
           </template>
         </q-chat-message>
@@ -30,24 +30,36 @@ import { Ref, watch } from 'vue'
 import { ref, computed, defineComponent, onUpdated } from 'vue'
 import getCollection from '@/composables/getCollection'
 import { formatDistanceToNow } from 'date-fns'
+import nameEmojis from '../assets/nameEmojis'
 
 export default defineComponent({
   name: 'ChatWindow',
   setup() {
     const { error, documents } = getCollection('messages')
 
-    const formattedDocuments = computed(() => documents.value ? documents.value.map(doc => ({
-      ...doc, createdAt: formatDistanceToNow(doc.createdAt.toDate())
-    })) : null)
+    const formattedDocuments = computed(() => documents.value
+      ? documents.value.map(doc => ({
+        ...doc,
+        messages: [ doc.message ],
+        createdAt: formatDistanceToNow(doc.createdAt.toDate()),
+        emoji: nameEmojis[doc?.name?.[0]?.toUpperCase?.()] || nameEmojis['.']
+      })).reduce((prev, curr, i) => {
+        if (i === 0) return [ curr ]
+        const prevMessage = prev[prev.length - 1]
+        if (prevMessage.name === curr.name) {
+          prevMessage.messages = [...prevMessage.messages, curr.message]
+          return prev
+        }
+        return [...prev, curr]
+      }, [])
+      : null)
 
     // Auto-scroll to bottom of messages
     const scrollArea: Ref<any> = ref(null)
 
     const scrollToBottom = () => {
       if (scrollArea.value) {
-        (window as any).vittu = scrollArea.value
         scrollArea.value.setScrollPercentage('vertical', 1, 300)
-        console.log('vittu')
       }
     }
 
